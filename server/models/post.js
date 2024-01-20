6efea29fe8cfe1a9107550ae77e334b6edd43e41
Aadil Ahmad Chattha <136930460+adichats92@users.noreply.github.com>
2024-01-20 23:46:56 +0100

    trying authentication user model etc

--- server/models/post.js
+++ server/models/post.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema(
 	{
-		userName: { type: String, required: [true, 'user name is required'] },
-		body: { type: String, required: [true, 'post content is required'] },
+		userName: { type: String, required: [true, 'User name is required'] },
+		body: { type: String, required: [true, 'Post content is required'] },
 		comments: [{ body: String, date: Date }],
 		date: { type: Date, default: Date.now },
 		hidden: Boolean,
@@ -11,4 +11,4 @@ const postSchema = new mongoose.Schema(
 	},
 	{ timestamps: true }
 );
-module.exports = mongoose.model('Post', postSchema)
+module.exports = mongoose.model('Post', postSchema);
---
+++ server/models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+	{
+		userName: {
+			type: String,
+			required: [true, 'User name is required'],
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: [true, 'Email address is required'],
+		},
+		password: {
+			type: String,
+			required: [true, 'Password is required'],
+			minLength: [8, 'Password MUST be at least 8 characters'],
+		},
+	},
+	{ timestamps: true }
+);
+//For checking unique email
+userSchema.path('email').validate(async (value) => {
+	const emailCount = await mongoose.models.User.countDocuments({
+		email: value,
+	});
+	return !emailCount;
+}, 'Email already exists');
+//Virtual field to store info from req but not in DB
+userSchema
+	.virtual('confirmPassword')
+	.get(() => this._confirmPassword)
+	.set((value) => (this._confirmPassword = value));
+
+//Pre Validation before DB interaction
+//For Password
+userSchema.pre('validate', function (next) {
+	if (this.password !== this.confirmPassword) {
+		this.invalidate('confirmPassword', 'Passwords must match');
+		console.log("Passwords  don't match");
+	}
+	next();
+});
+//And
+userSchema.pre('save', async function (next) {
+	console.log('in pre save');
+	try {
+		const hashedPassword = await bcrypt.hash(this.password, 10);
+		console.log('HASHED', hashedPassword);
+		this.password = hashedPassword;
+	} catch (error) {
+		console.log('There is an error', error);
+	}
+});
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
