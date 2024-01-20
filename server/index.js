34341e7b414a72230ba797786c96c68d9bbcb9e7
Aadil Ahmad Chattha <136930460+adichats92@users.noreply.github.com>
2024-01-20 21:25:08 +0100

    ready for rendering

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 const postsRouter = require('./routes/posts');
 const PORT = process.env.PORT || 4000;
@@ -8,8 +9,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-//All routes under here
+//All main routes under here
 app.use('/api/posts', postsRouter);
+
+//All main routes above this point
+if (process.env.NODE_ENV === 'production') {
+	const buildPath = path.join(__dirname, '../client/dist');
+	app.use(express.static(buildPath));
+	//This gets frontend routes for deployment
+	app.get('*', (req, res) => res.sendFile(path.join(buildPath, 'index.html')));
+}
 
 connectDB().then(() => {
 	app.listen(PORT, () => {
