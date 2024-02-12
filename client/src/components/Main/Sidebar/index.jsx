623d9cdf97332d7972586210e95a76dfc26378bb
Aadil Ahmad Chattha <136930460+adichats92@users.noreply.github.com>
2024-02-12 18:33:10 +0100

    ui

--- client/src/components/Main/Sidebar/index.jsx
+++ client/src/components/Main/Sidebar/index.jsx
@@ -4,7 +4,7 @@ import Todos from './Todos';
 const SideBar = () => {
 	return (
 		<>
-			<div className='bg-white bg-opacity-20 backdrop-blur-md dark:text-white min-w-96 fixed top-16 left-0 h-full z-40'>
+			<div className='bg-white bg-opacity-20 backdrop-blur-md dark:text-white min-w-96 fixed top-16 left-0 h-full'>
 				<div>
 					<Radio />
 					<Todos />
